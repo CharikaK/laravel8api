a5173a4644af4e5fdc4390237edf2f842f9dd740
CharixKalpi <chooti@gmail.com>
2021-05-04 23:39:47 +0100

    working with view - passing values with with() - v2

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,7 +9,13 @@ class ProductsController extends Controller
     public function index(){
         $title = 'Welcome to Laravel 8';
         $createdBy = 'Charika';
-        return view('products.view',compact('title','createdBy'));
+        
+        
+        // Compact ()
+        // return view('products.view',compact('title','createdBy'));
+
+        //with() - best when passing one variable
+        return view('products.view')->with('title',$title)->with('createdBy',$createdBy);
     }
 
     public function about(){
